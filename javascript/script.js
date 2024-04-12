64a673add874623bf723920c9aa98e1fe59f7b4d
andirosad22 <andirosad22@gmail.com>
2024-04-12 08:41:13 +0700

    modivication skill in script.js and index.html"

--- javascript/script.js
+++ javascript/script.js
@@ -1,6 +1,6 @@
 /* ====================== Typing animation ============ */
 var typed = new Typed(".typing", {
-  strings:["", "Web Designer", "Web Developer", "Graphic Designer", "YouTuber"],
+  strings:["", "Frond End Developer", "Back End Developer", "Graphic Designer"],
   typeSpeed:100,
   BackSpeed:60,
   loop: true
